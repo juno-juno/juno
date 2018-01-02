e34b20992fe720eeb218836f095876587b7722e2
juno-dev <junoophoto@gmail.com>
2018-01-02 17:07:52 +1300

    Initial commit

---
+++ contactthanks.php
@@ -0,0 +1,202 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Junø</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html"><strong>Thanks! Your message has been sent!</a></h1>
+						<nav>
+							<ul>
+								<li><a href="#footer" class="icon fa-info-circle">Contact</a></li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+						<article class="thumb">
+							<a href="images/fulls/01.jpg" class="image"><img src="images/thumbs/01.jpg" alt="" /></a>
+							<h2>Guns &amp; Oil</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://gunsandoil.com" target="_blank">Check out the website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/02.jpg" class="image"><img src="images/thumbs/02.jpg" alt="" /></a>
+							<h2>Powershop</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://powershop.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/03.jpg" class="image"><img src="images/thumbs/03.jpg" alt="" /></a>
+							<h2>Line 7</h2>
+							<p>Design &amp; Development &amp; Product Photography</p>
+							<p><a href="http://marine.line7.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/04.jpg" class="image"><img src="images/thumbs/04.jpg" alt="" /></a>
+							<h2>Flyingfox</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://flyingfox.net.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/05.jpg" class="image"><img src="images/thumbs/05.jpg" alt="" /></a>
+							<h2>Belmoto</h2>
+							<p>Design &amp; Development &amp; Product Photography</p>
+							<p><a href="http://belmoto.gt" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/06.jpg" class="image"><img src="images/thumbs/06.jpg" alt="" /></a>
+							<h2>Nutters</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://nutters.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/07.jpg" class="image"><img src="images/thumbs/07.jpg" alt="" /></a>
+							<h2>Results</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="https://www.results.com" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/08.jpg" class="image"><img src="images/thumbs/08.jpg" alt="" /></a>
+							<h2>Borderless</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://www.borderless.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/09.jpg" class="image"><img src="images/thumbs/09.jpg" alt="" /></a>
+							<h2>Nandos</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="https://www.nandos.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/10.jpg" class="image"><img src="images/thumbs/10.jpg" alt="" /></a>
+							<h2>Wine-Searcher</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://wine-searcher.com" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/11.jpg" class="image"><img src="images/thumbs/11.jpg" alt="" /></a>
+							<h2>Animates</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://www.animates.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/12.jpg" class="image"><img src="images/thumbs/12.jpg" alt="" /></a>
+							<h2>iDEA Starter</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://ideastarter.co.nz" target="_blank">Check out website</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/13.jpg" class="image"><img src="images/thumbs/13.jpg" alt="" /></a>
+							<h2>Welly Weather app</h2>
+							<p>Design &amp; Development &amp; Photography</p>
+							<p><a href="http://junophotography.co/dev/weather/" target="_blank">Check out the app</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/14.jpg" class="image"><img src="images/thumbs/14.jpg" alt="" /></a>
+							<h2>Calculator app</h2>
+							<p>Design &amp; Development</p>
+							<p><a href="http://junophotography.co/dev/calculator/" target="_blank">Check out the app</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/15.jpg" class="image"><img src="images/thumbs/15.jpg" alt="" /></a>
+							<h2>Wine-Searcher App</h2>
+							<p>Screen Design</p>
+							<p><a href="https://itunes.apple.com/nz/app/wine-searcher/id599836194?mt=8" target="_blank">Check out the app</a></p>
+						</article>
+
+						<article class="thumb">
+							<a href="images/fulls/16.jpg" class="image"><img src="images/thumbs/16.jpg" alt="" /></a>
+							<h2>Junø Photography</h2>
+							<p>Design &amp; Development &amp; Photography</p>
+							<p><a href="http://junophotography.co/" target="_blank">Check out website</a></p>
+						</article>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer" class="panel">
+						<div class="inner split">
+							<div>
+								<section>
+									<h2>Hi I'm Junø</h2>
+									<p>
+										I'm a Senior Front-end Developer with a passion for producing clean code and beautiful, interactive static and dynamic websites and applications. I'm available for freelance projects.
+									</p>
+									<p>
+										I use with HTML(5), CSS(3), JavaScript &amp; PHP.
+										I love coding things from scratch, but I can work with front-end frameworks like Bootstrap and Foundation too.
+									</p>
+								</section>
+								<section>
+									<h2>Follow me on ...</h2>
+									<ul class="icons">
+										<li><a href="https://www.facebook.com/junoophotography/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="https://www.instagram.com/juno.photography/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="https://github.com/PixxelCraft" class="icon fa-github"><span class="label">GitHub</span></a></li>
+										<li><a href="https://www.linkedin.com/in/june-brunken-30779048/" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									</ul>
+								</section>
+								<p class="copyright">
+									&copy; Junø. Design: <a href="http://junophotography.co">Junø</a>.
+								</p>
+							</div>
+
+							<div id="contact-area">
+								<section>
+									<h2>Get in touch</h2>
+									<form method="post" action="contactengine.php">
+										<div class="field half first">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="field half">
+											<input type="text" name="email" id="email" placeholder="Email" />
+										</div>
+										<div class="field">
+											<textarea name="message" id="message" rows="4" placeholder="Message"></textarea>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send" class="special" /></li>
+										</ul>
+									</form>
+								</section>
+							</div>
+
+						</div>
+					</footer>
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
